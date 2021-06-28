85202a31c5f0f317378b313682074034b07ebbe1
William Stull II <williamstull@gmail.com>
2021-06-28 10:33:32 -0700

    mvp;

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,5 +1,4 @@
 const Ticket = require('../models/ticket');
-const Flight = require('../models/flight')
 
 module.exports = {
     new: newTicket,
@@ -7,17 +6,16 @@ module.exports = {
 }
 
 function newTicket(req, res) {
-    let flightId = req.params.id
+    let flightId = req.params.id;
+    console.log('1' + flightId);
     res.render('tickets/new', {title: 'Add Ticket', flightId})
 }
 
 function create(req, res) {
-    let ticket = new Ticket(req.body); 
-    console.log(req.params.id);
-    Flight.findById(req.params.id, function(err, flight) {
-        ticket.flight = flight;
-    })
-    console.log(ticket);
-    Ticket.push(ticket);
-    res.redirect(`/flights/${req.params.id}`)
+    req.body.flight = req.params.id;
+    let ticket = new Ticket(req.body);
+    ticket.save(function(err) {
+        if (err) return;
+        res.redirect(`/flights/${req.params.id}`)
+    });
 }
--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/', ticketsRouter)
 app.use('/flights', flightsRouter);
 app.use('/', destinationsRouter);
-app.use('/', ticketsRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
